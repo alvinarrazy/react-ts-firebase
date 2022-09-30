8bf0b09eda1a791c136cf80001ea8f75cf1ac9a5
alvinarrazy <alvinarrazy29@gmail.com>
2022-09-30 14:28:48 +0700

    added bootstrap

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWEV5fFK2cH9PoNrdP66hETDQ_ULF_Csg",
+  authDomain: "sandbox-68162.firebaseapp.com",
+  projectId: "sandbox-68162",
+  storageBucket: "sandbox-68162.appspot.com",
+  messagingSenderId: "299329872864",
+  appId: "1:299329872864:web:a34e93bb45363c1e760185"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
